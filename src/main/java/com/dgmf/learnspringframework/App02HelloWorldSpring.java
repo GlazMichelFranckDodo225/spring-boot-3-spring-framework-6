e9dabe56b3722317b2c8148a6b1ce2d773bc71b6
FranckDodo91 <devlogappdgmf@gmail.com>
2023-03-31 19:21:10 +0200

    Using the 'record' keyword in the HelloWorldConfiguration configuration class

--- src/main/java/com/dgmf/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/dgmf/learnspringframework/App02HelloWorldSpring.java
@@ -12,6 +12,9 @@ public class App02HelloWorldSpring {
 		// - name (method) (@Bean)
 		// 3 - Retrieving Beans manage by Spring
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("address"));
 
 	}
 
--- src/main/java/com/dgmf/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/dgmf/learnspringframework/HelloWorldConfiguration.java
@@ -3,10 +3,33 @@ package com.dgmf.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Eliminate verbosity in creating Java Bean
+// Public accessor methods, constructor, equals, hashcode
+// and toString are automatically created.
+// Released in JDK 16
+record Person(String name, int age) {};
+record Address(String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 	@Bean
 	public String name() {
-		return "I am a Bean";
+		return "Ranga";
+	}
+	
+	@Bean
+	public int age() {
+		return 15;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 20);
+		
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Baker Street", "London");
 	}
 }
