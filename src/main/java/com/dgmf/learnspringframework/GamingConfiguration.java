3d2e64bed8a3d9032b6ba6ebb84f139df303468e
FranckDodo91 <devlogappdgmf@gmail.com>
2023-04-22 07:02:18 +0200

    Using the 'try with resources' feature in the 'App02HelloWorldSpring' class and creating the 'App03GamingSpringBeans' and 'GamingConfiguration' classes

---
+++ src/main/java/com/dgmf/learnspringframework/GamingConfiguration.java
@@ -0,0 +1,15 @@
+package com.dgmf.learnspringframework;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.dgmf.learnspringframework.game.GameRunner;
+import com.dgmf.learnspringframework.game.PacmanGame;
+
+@Configuration
+public class GamingConfiguration {
+	// var game = new MarioGame();
+	// var game = new SuperContraGame();
+	//	var game = new PacmanGame();
+	//	var gameRunner = new GameRunner(game);
+	//	gameRunner.run();
+}
--- src/main/java/com/dgmf/learnspringframework/helloworld/App02HelloWorldSpring.java
+++ src/main/java/com/dgmf/learnspringframework/helloworld/App02HelloWorldSpring.java
@@ -9,24 +9,25 @@ public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
 		// 1 - Launch a Spring Context (IoC Container )
-		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-		// 2 - Configure the Objects that we want Spring to manage 
-		// - HelloWorldConfiguration (@Configuration)
-		// - name (method) (@Bean)
-		// 3 - Retrieving Beans manage by Spring
-		System.out.println(context.getBean("name"));
-		System.out.println(context.getBean("age"));
-		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("person2MethodCall"));
-		System.out.println(context.getBean("person3Parameters"));
-		System.out.println(context.getBean(Person.class));
-		System.out.println(context.getBean(Address.class));
-		System.out.println(context.getBean("person5Qualifier"));
-//		System.out.println(context.getBean("address3"));
-		
-		// Afficher les noms de tous les Beans créés dans l'application
-		// context.getBeanDefinitionNames() ==> retourne String[]
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);;
+		try (var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class)) {
+			// 2 - Configure the Objects that we want Spring to manage
+			// - HelloWorldConfiguration (@Configuration)
+			// - name (method) (@Bean)
+			// 3 - Retrieving Beans manage by Spring
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
+			System.out.println(context.getBean("person"));
+			System.out.println(context.getBean("person2MethodCall"));
+			System.out.println(context.getBean("person3Parameters"));
+			System.out.println(context.getBean(Person.class));
+			System.out.println(context.getBean(Address.class));
+			System.out.println(context.getBean("person5Qualifier"));
+			// System.out.println(context.getBean("address3"));
+			// Afficher les noms de tous les Beans créés dans l'application
+			// context.getBeanDefinitionNames() ==> retourne String[]
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			;
+		}
 
 	}
 
