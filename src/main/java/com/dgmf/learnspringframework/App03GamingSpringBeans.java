7d1386ea1eee22fd99875532969ea799270844fe
FranckDodo91 <devlogappdgmf@gmail.com>
2023-04-23 04:08:27 +0200

    Creating a 'game' Bean in the 'GamingConfiguration' config class

--- src/main/java/com/dgmf/learnspringframework/App03GamingSpringBeans.java
+++ src/main/java/com/dgmf/learnspringframework/App03GamingSpringBeans.java
@@ -1,18 +1,14 @@
 package com.dgmf.learnspringframework;
 
-import com.dgmf.learnspringframework.game.GameRunner;
-import com.dgmf.learnspringframework.game.MarioGame;
-import com.dgmf.learnspringframework.game.PacmanGame;
-import com.dgmf.learnspringframework.game.SuperContraGame;
-
 public class App03GamingSpringBeans {
 
 	public static void main(String[] args) {
+		
 		// var game = new MarioGame();
 		// var game = new SuperContraGame();
-		var game = new PacmanGame();
-		var gameRunner = new GameRunner(game);
-		gameRunner.run();
+//		var game = new PacmanGame();
+//		var gameRunner = new GameRunner(game);
+//		gameRunner.run();
 		
 
 	}
--- src/main/java/com/dgmf/learnspringframework/GamingConfiguration.java
+++ src/main/java/com/dgmf/learnspringframework/GamingConfiguration.java
@@ -1,12 +1,21 @@
 package com.dgmf.learnspringframework;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.dgmf.learnspringframework.game.GameRunner;
+import com.dgmf.learnspringframework.game.GamingConsole;
 import com.dgmf.learnspringframework.game.PacmanGame;
 
 @Configuration
 public class GamingConfiguration {
+	@Bean
+	public GamingConsole game() {
+		var game = new PacmanGame();
+		
+		return game;
+	}
+	
 	// var game = new MarioGame();
 	// var game = new SuperContraGame();
 	//	var game = new PacmanGame();
