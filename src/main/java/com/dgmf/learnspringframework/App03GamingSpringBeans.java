79d6750e5ea9985770a17d43e604f3c2574b9ad0
FranckDodo91 <devlogappdgmf@gmail.com>
2023-04-23 04:24:28 +0200

    Added 'GamingConfiguration' configuration class to IoC container. Test using the Bean 'game' ==> Ok.

--- src/main/java/com/dgmf/learnspringframework/App03GamingSpringBeans.java
+++ src/main/java/com/dgmf/learnspringframework/App03GamingSpringBeans.java
@@ -1,15 +1,21 @@
 package com.dgmf.learnspringframework;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.dgmf.learnspringframework.game.GamingConsole;
+
 public class App03GamingSpringBeans {
 
 	public static void main(String[] args) {
-		
+		try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+			context.getBean(GamingConsole.class).up();
+		}
+
 		// var game = new MarioGame();
 		// var game = new SuperContraGame();
 //		var game = new PacmanGame();
 //		var gameRunner = new GameRunner(game);
 //		gameRunner.run();
-		
 
 	}
 
