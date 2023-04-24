d4f4d5547222ee7aaaf9a268b97ee369366ed91e
FranckDodo91 <devlogappdgmf@gmail.com>
2023-04-24 04:22:06 +0200

    Launching the GameRunner Bean in the Driver App03GamingSpringBeans class

--- src/main/java/com/dgmf/learnspringframework/App03GamingSpringBeans.java
+++ src/main/java/com/dgmf/learnspringframework/App03GamingSpringBeans.java
@@ -2,6 +2,7 @@ package com.dgmf.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.dgmf.learnspringframework.game.GameRunner;
 import com.dgmf.learnspringframework.game.GamingConsole;
 
 public class App03GamingSpringBeans {
@@ -9,13 +10,8 @@ public class App03GamingSpringBeans {
 	public static void main(String[] args) {
 		try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
 			context.getBean(GamingConsole.class).up();
+			context.getBean(GameRunner.class).run();
 		}
-
-		// var game = new MarioGame();
-		// var game = new SuperContraGame();
-//		var game = new PacmanGame();
-//		var gameRunner = new GameRunner(game);
-//		gameRunner.run();
 
 	}
 
