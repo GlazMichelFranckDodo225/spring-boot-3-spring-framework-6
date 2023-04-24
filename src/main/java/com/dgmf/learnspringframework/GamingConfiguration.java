894d775326f52cfdc33c361af86ea2763e368069
FranckDodo91 <devlogappdgmf@gmail.com>
2023-04-24 04:13:24 +0200

    Creation of a Bean gameRunner() taking as parameter a Bean game() of Type GamingConsole in the configuration class GamingConfiguration

--- src/main/java/com/dgmf/learnspringframework/GamingConfiguration.java
+++ src/main/java/com/dgmf/learnspringframework/GamingConfiguration.java
@@ -12,13 +12,15 @@ public class GamingConfiguration {
 	@Bean
 	public GamingConsole game() {
 		var game = new PacmanGame();
-		
+
 		return game;
 	}
-	
-	// var game = new MarioGame();
-	// var game = new SuperContraGame();
-	//	var game = new PacmanGame();
-	//	var gameRunner = new GameRunner(game);
-	//	gameRunner.run();
+
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner = new GameRunner(game);
+
+		return gameRunner;
+	}
+
 }
