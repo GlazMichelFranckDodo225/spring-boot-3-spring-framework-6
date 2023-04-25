5830388fbc4037d3e5148128574d65fa14f682f5
FranckDodo91 <devlogappdgmf@gmail.com>
2023-04-25 05:46:53 +0200

    Reactoring: transfer of the configuration class code 'GamingConfiguration' into the driver class file 'App03GamingSpringBeans'. Deleting the 'GamingConfiguration' file

--- src/main/java/com/dgmf/learnspringframework/App03GamingSpringBeans.java
+++ src/main/java/com/dgmf/learnspringframework/App03GamingSpringBeans.java
@@ -1,9 +1,30 @@
 package com.dgmf.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.dgmf.learnspringframework.game.GameRunner;
 import com.dgmf.learnspringframework.game.GamingConsole;
+import com.dgmf.learnspringframework.game.PacmanGame;
+
+@Configuration
+class GamingConfiguration {
+	@Bean
+	public GamingConsole game() {
+		var game = new PacmanGame();
+
+		return game;
+	}
+
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner = new GameRunner(game);
+
+		return gameRunner;
+	}
+
+}
 
 public class App03GamingSpringBeans {
 
