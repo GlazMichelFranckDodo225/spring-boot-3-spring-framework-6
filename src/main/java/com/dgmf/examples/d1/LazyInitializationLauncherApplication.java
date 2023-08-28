b3bd695b5604ed70459d166bf2141d77d08f00c6
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-28 09:26:30 +0200

    Lazy and Eager Initialization of Spring Framework Beans

---
+++ src/main/java/com/dgmf/examples/d1/LazyInitializationLauncherApplication.java
@@ -0,0 +1,49 @@
+package com.dgmf.examples.d1;
+
+import com.dgmf.examples.a1.DependencyInjectionLauncherApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class ClassA {
+
+}
+
+@Component
+@Lazy
+class ClassB {
+	private ClassA classA;
+
+	public ClassB(ClassA classA) {
+		// Logic
+		System.out.println("Some Initialization Logic");
+		this.classA = classA;
+	}
+
+	public void doSomething() {
+		System.out.println("Do Something ...");
+	}
+}
+
+@Configuration
+@ComponentScan
+public class LazyInitializationLauncherApplication {
+	public static void main(String[] args) {
+		try (var context =
+					 new AnnotationConfigApplicationContext(
+							 LazyInitializationLauncherApplication.class
+					 )
+			) {
+			System.out.println("Initialization of the Context is completed");
+
+			context.getBean(ClassB.class).doSomething();
+		}
+
+	}
+
+}
